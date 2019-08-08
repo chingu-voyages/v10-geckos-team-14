910a97550c8c04b56d28e308436a46dea0b98060
Kathy * <Kathy@KathyLambert.me>
2019-08-07 20:53:28 -0400

    Create loginSuccess page and move payment text to loginSuccess. Correct all old payment routes to new loginSuccess page.  Add new route for order confirmaition

--- app.js
+++ app.js
@@ -216,6 +216,20 @@ app.get('/payment',function(req,res){
 		res.redirect('/login')
 	}	
 })
+//Get request: LOGIN SUCCESS PAGE=============================================
+app.get('/loginSuccess',function(req,res){
+	
+	if (req.isAuthenticated()){
+		navCheck=true
+		Client.findOne({username:clientDisplayName}, function(err, foundClient){
+			res.render('loginSuccess',{
+			clientDisplayName:'Hi '+foundClient.clientFirstName+'!'					
+			})
+		})
+	}else{
+		res.redirect('/login')
+	}	
+})
 //Get request: LOGOUT ROUTE=============================================
 app.get('/logout', function(req, res){
 	navCheck=false
@@ -295,7 +309,7 @@ app.post('/selectedFixer', function(req,res){
 				if (err) {
 					console.log(err)
 				} else {
-					res.redirect('/')
+					res.redirect('/payment')
 				}
 			})
 		}
@@ -331,7 +345,7 @@ app.post('/register', function(req,res){
 						console.log(err)
 					}
 				})
-				res.redirect('/payment')
+				res.redirect('/loginSuccess')
 				clientDisplayName= req.body.username
 				console.log('working')
 			})
@@ -368,7 +382,7 @@ app.post('/login', function(req,res){
 			}else{
 				clientDisplayName= req.body.username
 				console.log('clientName :'+ clientDisplayName)
-				return	res.redirect('/payment')
+				return	res.redirect('/loginSuccess')
 			}
 		})
 	}) (req,res)
